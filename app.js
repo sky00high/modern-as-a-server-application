7bc83e41c69227c3850d1b5c5365a0f2e3feca88
sky00high <tianyu0726@gmail.com>
2017-02-02 21:40:19 -0500

    add /login endpoint

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var jwt = require('jsonwebtoken');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -22,8 +22,31 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+//app.use('/', index);
 app.use('/users', users);
+
+var router = express.Router();
+router.get('/', function(req,res){
+  res.json({ message: 'lalalalala2;'});
+});
+
+router.route('/login')
+  .post(function(req, res) {
+    
+    var token = jwt.sign(req.body, 'secret', {
+      expiresIn:60*60*24
+    });
+    res.json({
+      success:true,
+      message: 'this is the tokken',
+      token:token
+    });
+  })
+  .get(function(req,res){
+    res.json({ message: 'lalalalala;'});
+    console.log(req);
+  });
+app.use('/', router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
