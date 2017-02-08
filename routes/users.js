31c52ab05711a26a5456987252a24a63dbc585a8
sky00high <tianyu0726@gmail.com>
2017-02-08 18:35:20 -0500

    users done

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 var AWS = require('aws-sdk');
-console.log(process);
-AWS.config.region = process.env.REGION;
-var userTable = process.env.userTable;
+AWS.config.region = 'us-east-1';
+//var userTable = process.env.userTable;
+var userTable = 'UserTable';
 var ddb = new AWS.DynamoDB();
 
 /* GET users listing. */
@@ -13,12 +13,45 @@ router.get('/', function(req, res, next) {
 	var params = {
 		TableName: userTable
 	};
-	console.log(params);
-	ddb.getItem(params, function(err, data){
-		if(err) console.log(err);
-		else console.log(data);
+
+
+	//TODO beautify returned json structure
+	ddb.scan(params, function(err, data){
+		if(err) res.status(500).send({ error: 'user, get, dynamo'});
+		else res.json(data.Items);
 	})
-  res.json(process.env);
+});
+
+router.post('/', function(req, res, next){
+	if(!req.body.username) res.status(400).send({error:"no username provided"});
+	if(!req.body.username) res.status(400).send({error:"no password provided"});
+
+
+	var item = {
+		'username' : {'S' : req.body.username},
+		'password' : {'S' : req.body.password}   
+	};
+	ddb.putItem({
+		'TableName' : userTable,
+		'Item' : item,
+		'Expected': { username: { Exists: false } }     
+	}, function(err, data){
+		if (err) {
+      var returnStatus = 500;
+
+      if (err.code === 'ConditionalCheckFailedException') {
+        returnStatus = 409;
+      }
+
+      res.status(returnStatus).end();
+      console.log('DDB Error: ' + err);
+  	} else {
+  		res.status(201).end();
+  	}
+  });
+
+
+
 });
 
 module.exports = router;
